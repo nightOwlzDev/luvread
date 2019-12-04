a6e544cd1663b4b7fb187dbe933da7af85b7487b
NightNB <nightt35739@gmail.com>
2019-12-04 14:47:24 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Container from '@material-ui/core/Container';
+
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Container maxWidth="sm">
+                    
+                </Container>
+            </div>
+
+        )
+    }
+}
+
+export default Header
